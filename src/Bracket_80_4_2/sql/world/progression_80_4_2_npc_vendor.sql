c0edad2a5eda1d1942bf18cf45d5590e3bf67861
Sven Bledt <sven.bledt@gmail.com>
2023-09-09 01:28:54 +0200

    custom(Add): Cloaks for custom serrver

---
+++ src/Bracket_80_4_2/sql/world/progression_80_4_2_npc_vendor.sql
@@ -0,0 +1,7 @@
+-- cloaks to the vendor
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES (1000002, 0, 102245, 0, 0, 80003, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES (1000002, 0, 102246, 0, 0, 80003, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES (1000002, 0, 102247, 0, 0, 80003, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES (1000002, 0, 102248, 0, 0, 80003, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES (1000002, 0, 102249, 0, 0, 80003, 0);
+REPLACE INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `VerifiedBuild`) VALUES (1000002, 0, 102250, 0, 0, 80003, 0);
